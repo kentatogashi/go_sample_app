c967df0a2b97438a528f0a0d2536679730f84da9
togashik <togashik@togashi.k>
2015-10-12 17:23:58 +0900

    init

---
+++ server.go
@@ -0,0 +1,58 @@
+package main
+
+import "net/http"
+import "io/ioutil"
+import "text/template"
+
+type Page struct {
+	Title string
+	Body  []byte
+}
+
+func (p *Page) save() error {
+	filename := p.Title + ".txt"
+	return ioutil.WriteFile(filename, p.Body, 0600)
+}
+
+func loadPage(title string) (*Page, error) {
+	filename := title + ".txt"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	return &Page{Title: title, Body: body}, nil
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	p, _ := loadPage(title)
+
+	t, _ := template.ParseFiles("view.html")
+	t.Execute(w, p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[6:]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	t, _ := template.ParseFiles("edit.html")
+	t.Execute(w, p)
+}
+
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+  title := r.URL.Path[6:]
+  body := r.FormValue("body")
+  p := &Page{Title: title, Body: []byte(body)}
+  p.save()
+  http.Redirect(w, r, "/view/" + title, http.StatusFound)
+  }
+
+func main() {
+
+	http.HandleFunc("/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+  http.HandleFunc("/save/", saveHandler)
+	http.ListenAndServe(":8080", nil)
+}
